69f01452175531f191dc200747f6db02526c31be
congminh20 <phamcongminh201100@gmail.com>
2023-01-31 15:04:35 +0700

    completed vector implematation

--- main.cpp
+++ main.cpp
@@ -4,23 +4,21 @@
 
 using namespace std;
 
-class Student{
-
-};
 int main(int, char**) {
     
     Vector<int> v1;
     v1.push_back(5);
     v1.push_back(10);
+    v1.push_back(15);
     v1.push_back(20);
-    v1.push_back(30);
 
-    // v1.insert(v1.begin() + 4,15);
-    // for (auto it = v1.begin();
-    //      it != v1.end(); ++it)
-    //     cout << ' ' << *it;
-    // cout <<  v1.max_size() << endl;
-    vector<long long> v;
-    cout << v.max_size() << endl;
-    cout << sizeof(int) << endl;
+
+
+    // vector<int> v3{5,10,15,20};
+    // for(int i = 0;i < v3.size(); i++){
+    //     cout << v3[i] << "-";
+    // }
+
+
+
 }
--- vector.h
+++ vector.h
@@ -2,54 +2,79 @@
 #define VECTOR_H
 
 #include<iostream>
+#include<math.h>
+
 using namespace std;
+
 template <typename T>
 class Vector{
     private:
         T *data;
         int number;
+        int capacity;
         typedef T * iterator;
     public:
         //Constructor
         Vector(){
-            data = new T[1];
+            capacity = 1;
             number = 0;
+            data = new T[capacity];
             cout << "Call default constructor" << endl;
         };
 
         //Overload constructor
         Vector(const unsigned int index) {
-            number = index;
-            this->data = new T[number];
+            number = capacity = index;
+            this->data = new T[capacity];
+            // for(int i = 0; i < index; i++){
+            //     data[i] = NULL;
+            // }
             cout << "Call overloading constructor" << endl;
         };
 
         Vector(Vector &rhs);                    //Copy constructor
         Vector &operator=(const Vector &rhs);   //Assigmnet copy constructor
 
-        Vector(const Vector &&rhs);             //Move constructor
-        Vector &operator=(const Vector &&rhs);  //Move assignment constructor
+        Vector(Vector &&rhs);             //Move constructor
+        Vector &operator=(Vector &&rhs);  //Move assignment constructor
 
         T operator[](const int i);   //Access element by [] operator
         T & at(const unsigned int);   //Access element by method at()
 
-        void push_back(T);  //Push_back element(insert element)
-        void pop_back();    //Pop_back element(delete element)
-
-        int size();     //Size of vector
-        bool empty();   //IsEmpty
-
         T & front();  //Return frist element
         T & back();   //Return last element
 
+        int size();     //Return count of element in vector
+        size_t max_size();   //Return maximum element that vector can store 
+        bool empty();   //IsEmpty
+
+        void clear(){number = 0;}
         void insert(iterator, T);    //Insert element by position
+        void erase(iterator);
+
+        void push_back(T);  //Push_back element(insert element)
+        void pop_back();    //Pop_back element(delete element)
+
+        void push_front(T val);
+        void pop_front();
 
         iterator begin();   //Return an iterator pointing to the frist element of vector
         iterator end();     //Return an iterator pointing to the last element of vector
 
-        T max_size(){
-            return sizeof(data);
-        };
+
+        // Cout data's vector
+        friend ostream& operator<<(ostream &os,Vector &v){
+            os << "Size's vector: " << v.number << endl;
+            os << "Capacity's vector: " << v.capacity << endl;
+            os << "Elements in vector: " << endl;
+            for(int i = 0; i < v.size(); i++){
+                os << v.data[i] << " "; 
+            }
+            os << endl;
+            os << "The element front of vector is : " << v.front() << endl;
+            os << "The element back of vector is : " << v.back() << endl;
+            return os;
+        }
 };
 
 //=======================================
@@ -57,36 +82,53 @@ class Vector{
 template <typename T>
 Vector<T>::Vector(Vector &rhs)
 {
+    this->capacity = rhs.capacity;
     this->number = rhs.number;
-    data = new T[number];
-    *data = *rhs.data;
+    data = new T[capacity];
+    for(int i = 0; i < this->number; i++){
+        data[i] = rhs.data[i];
+    }
     cout << "Call copy constructor" << endl;
 }
 
 template <typename T>
 Vector<T> & Vector<T>::operator=(const Vector &rhs)
 {
+    this->capacity = rhs.capacity;
     this->number = rhs.number;
-    data = new T[number];
-    *data = *rhs.data;
+    data = new T[capacity];
+    for(int i = 0; i < this->number; i++){
+        data[i] = rhs.data[i];
+    }
     cout << "Call assignment copy constructor " << endl;
     return *this;
 };
 
 template <typename T>
-Vector<T>::Vector(const Vector &&rhs)
+Vector<T>::Vector(Vector &&rhs)
 {
-    *data = rhs.data;
+    this->capacity = rhs.capacity;
+    this->number = rhs.number;
+    data = new T[capacity];
+    for(int i = 0; i < this->number; i++){
+        data[i] = rhs.data[i];
+    }
     rhs.data = nullptr;
     cout << "Call move constructor" << endl;
 };
 
 template <typename T>
-Vector<T> & Vector<T>::operator=(const Vector &&rhs)
+Vector<T> & Vector<T>::operator=(Vector &&rhs)
 {
-    *data = rhs.data;
+    this->capacity = rhs.capacity;
+    this->number = rhs.number;
+    data = new T[capacity];
+    for(int i = 0; i < this->number; i++){
+        data[i] = rhs.data[i];
+    }
     rhs.data = nullptr;
-    cout << "Call move constructor" << endl;
+    cout << "Call assigment move constructor" << endl;
+    return *this;
 }
 
 template <typename T>
@@ -98,13 +140,17 @@ T Vector<T>::operator[](const int i){
 
 template <typename T>
 void Vector<T>::push_back(T d){
-    T *temp = new T[number++];
-    for(int i = 0;i < number - 1; i++){
-        temp[i] = data[i];
+    if(number == capacity){
+        T *temp = new T[capacity * 2];
+        for(int i = 0; i < number; i++){
+            temp[i] = data[i];
+        }
+        delete [] data;
+        data = temp;
+        capacity *= 2;
     }
-    delete [] data;
-    data = temp;
-    data[number - 1] = d;
+    data[number] = d;
+    number++;
 };
 
 template <typename T>
@@ -113,8 +159,46 @@ void Vector<T>::pop_back(){
 }
 
 template <typename T>
+void Vector<T>::push_front(T val){
+    if(number == capacity){
+        T *temp = new T[capacity * 2];
+        for(int i = 0; i < number; i++){
+            temp[i + 1] = data[i];
+        }
+        delete [] data;
+        data = temp;
+        capacity *= 2;
+        temp[0] = val;
+        number++;
+        return;
+    }
+
+    for(int i = number; i > 0; i--){
+        data[i] = data[i - 1];
+    }
+
+    data[0] = val;
+    number++;
+};
+
+template <typename T>
+void Vector<T>::pop_front(){
+    for(int i = 0; i < number - 1; i++){
+        data[i] = data[i + 1];
+    }
+    number--;
+}
+
+
+
+template <typename T>
 int Vector<T>::size(){
     return this->number;
+}
+
+template <typename T>
+size_t Vector<T>::max_size(){
+    return pow(2,64) / sizeof(T) / 2;
 }
 
 template <typename T>
@@ -141,25 +225,52 @@ T & Vector<T>::at(const unsigned int i){
 }
 
 template <typename T>
-void Vector<T>::insert(const iterator index, T val){
-    int j;
-    T * temp = new T[number++];
-
-    if(&data[number - 1] == index)
-        j = number - 1;
-
-    for(int i = 0; i < number - 1; i++){
-        temp[i] = data[i];
+void Vector<T>::erase(iterator index){
+    int i;
+    for(i = 0; i < number; i++){
         if(&data[i] == index)
-            j = i;
+            break;
+    }
+    if(i != number){
+        do{
+            data[i] = data[i + 1];
+        }while(i++ < number);
+    }
+    else{
+        throw runtime_error("double free or corruption (out)");
+        return;
     }
 
-    delete [] data;
-    data = temp;
-    for(int i = number - 1; i > j; i--){
+    number--;
+}
+
+template <typename T>
+void Vector<T>::insert(const iterator index, T val){
+    int j = -1;
+
+    for(int i = 0; i <= number; i++){
+        if(&data[i] == index){
+            j = i;
+            break;
+        }
+    }
+
+    if(number == capacity){
+        T *temp = new T(capacity * 2);
+        for(int i = 0; i < number; i++){
+            temp[i] = data[i];
+        }
+        delete [] data;
+        data = temp;
+        capacity *= 2;
+    }
+
+    for(int i = number; i > j; i--){
         data[i] = data[i - 1];
     }
+
     data[j] = val;
+    number++;
 }
 
 template <typename T>
